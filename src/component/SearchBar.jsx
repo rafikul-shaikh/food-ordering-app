5a519b7840a819ee7eecb94bd55b216bcd814305
Rafikul shaikh <rafikulshaikh510@gmail.com>
2025-12-20 14:55:16 +0530

    modified searchber and home files

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import { Search } from "lucide-react";
+import { useState, useEffect } from "react";
+
+const rotatingWords = [" 'Kebabs'", "'Fish Kebabs'", "'Veg Kebabs'"];
 
 const SearchBar = () => {
+  const [value, setValue] = useState("");
+  const [index, setIndex] = useState(0);
+
+  // Rotate placeholder text
+  useEffect(() => {
+    // stop rotation when user types
+    if (value.trim() !== "") return;
+
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % rotatingWords.length);
+    }, 2000); // change every 2 sec
+
+    return () => clearInterval(interval);
+  }, [value]);
+
+  const placeholderText =
+    value.trim() === "" ? `Search for ${rotatingWords[index]}` : "Search";
+
   return (
     <div className="w-full bg-white ">
       <div className="max-w-7xl mx-auto mb-10 px-35 py-3">
@@ -10,8 +31,9 @@ const SearchBar = () => {
           <Search size={20} className="absolute left-3 top-5" />
           <input
             type="text"
-            placeholder="Search for 'Veg Thali'"
-            aria-label="Search food items"
+            // value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            placeholder={placeholderText}
             className="w-full border border-gray-300 mb-4 px-12 py-4 rounded-md
                      focus:outline-none focus:ring-1 focus:ring-blue-300"
           />
@@ -20,5 +42,4 @@ const SearchBar = () => {
     </div>
   );
 };
-
 export default SearchBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,26 @@
-import React from "react";
 import Navbar from "../component/Navbar";
 import CategorySlider from "../component/CategorySlider";
 import SearchBar from "../component/SearchBar";
 import MenuSection from "../component/MenuSection";
 import Footer from "../component/Footer";
+import { useState } from "react";
+import { menuItems } from "../data/menuItems";
 
 const Home = () => {
+  const [searchText, setSearchText] = useState("");
+  const filteredItems = menuItems.filter(
+    (items) =>
+      items.name.toLowerCase().includes(searchText.toLowerCase()) ||
+      items.category.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <>
       <Navbar />
       <main className="pt-24">
         <CategorySlider />
-        <SearchBar />
-        <MenuSection />
+        <SearchBar searchText={searchText} setSearchText={searchText} />
+        <MenuSection items={filteredItems} />
         <Footer />
       </main>
     </>
